09fcd1772b2f8b7b28028b302e42bc22937adc7d
khulanJargalsaikhan <onagraa@gmail.com>
2021-11-17 22:21:22 -0500

    deploying to heroku

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -11,7 +11,7 @@ insert into employee (employee_id, first_name, last_name, email) values (nextval
 
 -- INSERT PROJECTS
 insert into project (project_id, name, stage, description) values (nextval('project_seq'), 'Large Production Deploy', 'NOTSTARTED', 'This requires all hands on deck for the final deployment of the software into production');
-insert into project (project_id, name, stage, description) values (nextval('project_seq'), 'Large Production Deploy', 'NOTSTARTED', 'This requires all hands on deck for the final deployment of the software into production');
+insert into project (project_id, name, stage, description) values (nextval('project_seq'), 'New Employee Budget',  'COMPLETED', 'Decide on a new employee bonus budget for the year and figureout who will be promoted');
 insert into project (project_id, name, stage, description) values (nextval('project_seq'), 'Office Reconstruction', 'INPROGRESS', 'The office building in Monroe has been damaged due to hurricane in the region. This needs to be reconstructed');
 insert into project (project_id, name, stage, description) values (nextval('project_seq'), 'Improve Intranet Security', 'INPROGRESS', 'With the recent data hack, the office security needs to be improved and proper security team needs to be hired for implementation');
 
